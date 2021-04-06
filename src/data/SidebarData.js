8f613bf8bd4907dd0eea904631e4e3d623a3c53f
Edinson Antolinez <edinson.antolinez@tus-cuentas.com>
2021-04-06 10:27:00 -0500

    cambios en sidebar

--- src/data/SidebarData.js
+++ src/data/SidebarData.js
@@ -1,33 +1,39 @@
-import MessageIcon from '@material-ui/icons/Message';
-import InboxIcon from '@material-ui/icons/Inbox';
-import DraftsIcon from '@material-ui/icons/Drafts';
-import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
-import PeopleIcon from '@material-ui/icons/People';
-import AppsIcon from '@material-ui/icons/Apps';
+import MessageIcon from "@material-ui/icons/Message";
+import InboxIcon from "@material-ui/icons/Inbox";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import PeopleIcon from "@material-ui/icons/People";
+import AppsIcon from "@material-ui/icons/Apps";
 
 export const sidebarItemsData = [
-    {
-        icon: <MessageIcon />,
-        text: "Thread"
-    },
-    {
-        icon: <InboxIcon />,
-        text: "All DMs"
-    },
-    {
-        icon: <DraftsIcon />,
-        text: "Mentions & Reactions"
-    },
-    {
-        icon: <BookmarkBorderIcon />,
-        text: "Save Items"
-    },
-    {
-        icon: <PeopleIcon />,
-        text: "Peoples & Groups"
-    },
-    {
-        icon: <AppsIcon />,
-        text: "More"
-    }
-]
+  {
+    icon: <MessageIcon />,
+    text: "Thread",
+    route: "threads",
+  },
+  {
+    icon: <InboxIcon />,
+    text: "All DMs",
+    route: "all-direct-messages",
+  },
+  {
+    icon: <DraftsIcon />,
+    text: "Mentions & Reactions",
+    route: "mentions-and-reactions",
+  },
+  {
+    icon: <BookmarkBorderIcon />,
+    text: "Save Items",
+    route: "saved-items",
+  },
+  {
+    icon: <PeopleIcon />,
+    text: "Peoples & Groups",
+    route: "groups-and-people",
+  },
+  {
+    icon: <AppsIcon />,
+    text: "More",
+    route: "more-options",
+  },
+];
